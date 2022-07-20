7293edb6693449d338448adafff0fef25de47542
edlavack <108073221+edlavack@users.noreply.github.com>
2022-07-20 08:38:53 -0400

    brian help

--- js/site.js
+++ js/site.js
@@ -1,14 +1,82 @@
-function getValue(){
-    alert("Hello World")
-}
-function getValue2(){
-    let msg= '';
-    msg=document.getElementById("message").value;
+//controller functions
+function getValues(){
 
-    Swal.fire(
-       { backdrop: false,
-        title: 'App Name',
-        text: msg,
-       }
-    );
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+    
+   
+
+
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
+
+
+    if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
+        let numbers = generateNumbers(1, 100);
+        displayValues(numbers);
+    } else {
+        alert('please enter a number')
+
+        Swal.fire(
+            { backdrop: false,
+             title: 'Uh Oh',
+             text: "You gotta enter numbers",
+            }
+         );
+    }
+}
+
+//business
+function generateNumbers(start, stop){
+let numbers = [];
+
+for (let i = start; i <= stop; i++) {
+    numbers.push(i);
+    
+}
+return numbers;
+}
+
+
+
+
+//view
+function displayValues(numArray){
+
+let results = document.getElementById("results")
+results.innerHTML = "";
+
+for (let i = 0; i < numArray.length; i++) {
+  
+
+    let div = document.createElement("div");
+
+    let currentNumber = numArray[i];
+    div.innerHTML = currentNumber;
+
+    div.classList.add("col");
+
+    
+    if (currentNumber % 3 == 0) {
+        div.classList.add("fizz")
+        div.innerHTML = "fizz";
+    }
+    
+    if (currentNumber % 5 == 0) {
+        div.classList.add("buzz")
+        div.innerHTML = "buzz";
+    }
+    
+    if (currentNumber % 3 == 0 && currentNumber % 5 == 0) {
+        div.classList.add("fizzbuzz")
+        div.innerHTML = "fizzbuzz";
+    }
+
+    
+
+    results.appendChild(div);
+}
+
+
+   
 }
